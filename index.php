9002d7fcd6783ac321c285f757bf40d694866a05
yueruiying <1334993200@qq.com>
2018-04-07 12:38:32 +0800

    修改首页的错误，并增加启动文件

--- index.php
+++ index.php
@@ -1,3 +1,13 @@
+<?php
+    session_start();
+    $msg = null;
+    if ($_SESSION && isset($_SESSION['nick_name'])) {
+        $msg = $_SESSION['company_name'] . '(' . $_SESSION['nick_name'] . ')';
+        $msg .= '<a href="quit.php">注销</a>';
+    } else {
+        $msg = '<a href="signin.php">登录去</a> or <a href="signup.php">注册</a>';
+    }
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,14 +17,6 @@
     <title>首页</title>
 </head>
 <body>
-    <?php
-        session_start();
-        if ($_SESSION && isset($_SESSION['nick_name'])) {
-            echo $_SESSION['company_name'] . '(' . $_SESSION['nick_name'] . ')';
-            echo '<a href="quit.php">注销</a>';
-        } else {
-            echo '<a href="signin.php">登录去</a> or <a href="signup.php">注册</a>';
-        }
-    ?>
+    <?php echo $msg ?>
 </body>
 </html>
---
+++ start.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# ./start.sh
+
+php -S 127.0.0.1:8081
